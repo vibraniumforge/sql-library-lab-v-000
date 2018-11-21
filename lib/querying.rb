64880bb2378a71e06c96a54b0738f74fb94a78f3
Greg Morgan <gregm9999@gmail.com>
2018-11-21 04:56:46 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,8 +1,8 @@
 def select_books_titles_and_years_in_first_series_order_by_year
   "
-  SELECT books.title, books.year FROM books
+  SELECT title, year FROM books
   WHERE series_id = 1
-  ORDER BY books.year
+  ORDER BY year
   ;"
 end
 
